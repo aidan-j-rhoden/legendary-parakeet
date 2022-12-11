88ba6752a928bdbae91199f813bd4be38426f0e3
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2022-12-10 18:02:33 -0600

    Added an info popup in lobby

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -2,7 +2,12 @@ extends Node
 
 func _ready():
 	get_node("v_box_container/button").connect("pressed", self, "_on_pressed")
-	
+	get_node("v_box_container/info").connect("pressed", self, "_on_info_pressed")
+
+
 func _on_pressed():
 	OS.set_window_size(Vector2(get_node("v_box_container/width").text, get_node("v_box_container/height").text))
 	OS.window_fullscreen = get_node("v_box_container/fullscreen").pressed
+
+func _on_info_pressed():
+	$"../info_pop".popup_centered_minsize()
